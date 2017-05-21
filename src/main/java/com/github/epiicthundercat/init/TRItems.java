862d5b210940aff540fd8c89f83e82cc3ee6daf7
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-05-21 03:59:32 -0700

    fix sword

--- src/main/java/com/github/epiicthundercat/init/TRItems.java
+++ src/main/java/com/github/epiicthundercat/init/TRItems.java
@@ -3,12 +3,14 @@ package com.github.epiicthundercat.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.epiicthundercat.item.TRItem;
+import com.github.epiicthundercat.item.TRItemSword;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -19,7 +21,7 @@ public class TRItems {
 	
 	public static List<Item> items = new ArrayList();
 	
-	public static Item unholy_sword = new TRItem("unholy_sword");
+	public static Item unholy_sword = new TRItemSword("unholy_sword", ToolMaterial.DIAMOND);
 	
 	
 	
--- src/main/java/com/github/epiicthundercat/item/TRItemSword.java
+++ src/main/java/com/github/epiicthundercat/item/TRItemSword.java
@@ -2,6 +2,7 @@ package com.github.epiicthundercat.item;
 
 import com.github.epiicthundercat.creativetabs.TRCreativeTab;
 import com.github.epiicthundercat.init.TRItems;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
@@ -11,40 +12,37 @@ import net.minecraft.item.ItemSword;
 import net.minecraft.potion.PotionEffect;
 
 public class TRItemSword extends ItemSword {
-    public TRItemSword(String name,ToolMaterial material) {
-        super(material);
-        setRegistryName(name.toLowerCase());
-        setUnlocalizedName(name.toLowerCase());
-        setCreativeTab(TRCreativeTab.RTabs);
-        addToItems(this);
-    }
-    private void addToItems(Item item) {
+	public TRItemSword(String name, ToolMaterial material) {
+		super(material);
+		setRegistryName(name.toLowerCase());
+		setUnlocalizedName(name.toLowerCase());
+		setCreativeTab(TRCreativeTab.RTabs);
+		addToItems(this);
+	}
 
-        TRItems.items.add(item);
+	private void addToItems(Item item) {
 
-    }
-@Override
-    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-    stack.damageItem(1, attacker);
-    if (target instanceof EntityLivingBase) {
-        if (stack.getItem() == TRItems.unholy_sword) {
-            ((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.WITHER, 200, 2, false, true));
-        }
+		TRItems.items.add(item);
 
-    }
-    return true;
+	}
+
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+		stack.damageItem(1, attacker);
+		if (target instanceof EntityLivingBase) {
+			if (stack.getItem() == TRItems.unholy_sword) {
+				((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.WITHER, 200, 2, false, true));
+			}
+
+		}
+		return true;
+	}
+
+	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+		if (toRepair.getItem() == TRItems.unholy_sword) {
+			return repair.getItem() == Items.NETHER_STAR;
+		}
+
+		return false;
+	}
 }
-
-
-
-
-    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
-        if (toRepair.getItem() == TRItems.unholy_sword) {
-            return repair.getItem() == Items.NETHER_STAR;
-        }
-
-        return false;
-    }
-}
-
-
