f119542d6b477819533d4be8680615939909ba81
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-05-21 02:48:53 -0700

    sync

--- src/main/java/com/github/epiicthundercat/proxy/ClientProxy.java
+++ src/main/java/com/github/epiicthundercat/proxy/ClientProxy.java
@@ -6,13 +6,14 @@ import com.github.epiicthundercat.entity.monster.EntityFallenAngel;
 import com.github.epiicthundercat.init.TRItems;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy{
 
-	
+	private static final Minecraft MC = Minecraft.getMinecraft();
 	public void preInit(FMLPreInitializationEvent preEvent) {
 		super.preInit(preEvent);
 		ModEntities.init();
@@ -35,4 +36,17 @@ public class ClientProxy extends CommonProxy{
 	public void registerEntities(FMLPreInitializationEvent preEvent) {
 		super.registerEntities(preEvent);
 	}
+	
+	
+	
+	public static void playSound(ISound sound) {
+		MC.getSoundHandler().playSound(sound);
+	}
+
+	
+	
+	public static void stopSound(ISound sound) {
+		MC.getSoundHandler().stopSound(sound);
+	}
+
 }
