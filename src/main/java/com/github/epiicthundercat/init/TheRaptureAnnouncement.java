0f015b0db8f530780cd208e28831bab6016fbd4d
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-05-21 20:42:07 -0700

    update

--- src/main/java/com/github/epiicthundercat/init/TheRaptureAnnouncement.java
+++ src/main/java/com/github/epiicthundercat/init/TheRaptureAnnouncement.java
@@ -23,86 +23,99 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TheRaptureAnnouncement {
-	public static void ScheduleNotice(String mainTxt, SoundEvent sound) {
-		// Temp fix for only having one notic
-		if (notices.size() == 0) {
-			notices.add(new RaptureChange(mainTxt, sound));
+
+	public static void ScheduleNotice(String mainTxt, String subTxt, String sound)
+	{
+		//Temp fix for only having one notic
+		if(notices.size() == 0)
+		{
+			notices.add(new QuestNotice(mainTxt, subTxt, sound));
 		}
 	}
-
-	static ArrayList<RaptureChange> notices = new ArrayList<RaptureChange>();
-
+	
+	static ArrayList<QuestNotice> notices = new ArrayList<QuestNotice>();
+	
 	@SubscribeEvent
-	public void onDrawScreen(RenderGameOverlayEvent.Post event) {
-		if (event.getType() != RenderGameOverlayEvent.ElementType.HELMET || notices.size() <= 0) {
+	public void onDrawScreen(RenderGameOverlayEvent.Post event)
+	{
+		if(event.getType() != RenderGameOverlayEvent.ElementType.HELMET || notices.size() <= 0)
+		{
 			return;
 		}
-
+		
 		Minecraft mc = Minecraft.getMinecraft();
 		ScaledResolution resolution = new ScaledResolution(mc);
 		int width = resolution.getScaledWidth();
 		int height = resolution.getScaledHeight();
-		RaptureChange notice = notices.get(0);
-
-		if (!notice.init) {
-			if (mc.isGamePaused() || mc.currentScreen != null) {
-				return; // Do not start showing a new notice if the player isn't
-						// looking
+		QuestNotice notice = notices.get(0);
+		
+		if(!notice.init)
+		{
+			if(mc.isGamePaused() || mc.currentScreen != null)
+			{
+				return; // Do not start showing a new notice if the player isn't looking
 			}
-
+			
 			notice.init = true;
 			notice.startTime = Minecraft.getSystemTime();
-			mc.getSoundHandler().playSound(
-					PositionedSoundRecord.getMasterRecord(TheRaptureSoundHandler.THE_RAPTURE_HAS_BEGUN, 1.0F));
+			mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(new SoundEvent(new ResourceLocation(notice.sound)), 1.0F));
 		}
-
-		if (notice.getTime() >= 6F) {
+		
+		if(notice.getTime() >= 6F)
+		{
 			notices.remove(0);
 			return;
 		}
-
+		
 		GlStateManager.pushMatrix();
-
-		float scale = width > 600 ? 1.5F : 1F;
-
+		
+		float scale = width > 600? 1.5F : 1F;
+		
 		GlStateManager.scale(scale, scale, scale);
-		width = MathHelper.ceil(width / scale);
-		height = MathHelper.ceil(height / scale);
-
-		float alpha = notice.getTime() <= 4F ? Math.min(1F, notice.getTime()) : Math.max(0F, 5F - notice.getTime());
+		width = MathHelper.ceil(width/scale);
+		height = MathHelper.ceil(height/scale);
+		
+		float alpha = notice.getTime() <= 4F? Math.min(1F, notice.getTime()) : Math.max(0F, 5F - notice.getTime());
 		alpha = MathHelper.clamp(alpha, 0.02F, 1F);
 		int color = new Color(1F, 1F, 1F, alpha).getRGB();
-
+		
 		GlStateManager.color(1F, 1F, 1F, alpha);
-
+		
 		GlStateManager.enableBlend();
 		GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
-
-		String tmp = TextFormatting.RED + "" + TextFormatting.UNDERLINE + "" + TextFormatting.BOLD
-				+ I18n.format(notice.mainTxt);
+     	
+		String tmp = TextFormatting.RED + "" + TextFormatting.UNDERLINE + "" + TextFormatting.BOLD + I18n.format(notice.mainTxt);
 		int txtW = mc.fontRendererObj.getStringWidth(tmp);
-		mc.fontRendererObj.drawString(tmp, width / 2 - txtW / 2, height / 4, color, true);
-
+		mc.fontRendererObj.drawString(tmp, width/2 - txtW/2, height/4, color, true);
+		
+		tmp = TextFormatting.YELLOW + "" + I18n.format(notice.subTxt);
+		txtW = mc.fontRendererObj.getStringWidth(tmp);
+		mc.fontRendererObj.drawString(tmp, width/2 - txtW/2, height/4 + 12, color, true);
+		
+		//GlStateManager.disableBlend();
 		GlStateManager.popMatrix();
 	}
-
-	public static class RaptureChange {
+	
+	public static class QuestNotice
+	{
 		long startTime = 0;
 		public boolean init = false;
 		public String mainTxt = "";
-
+		public String subTxt = "";
 		public ItemStack icon = null;
-		private SoundEvent sound;
-
-		public RaptureChange(String mainTxt, SoundEvent sound) {
+		public String sound = "THE_RAPTURE_HAS_BEGUN";
+		
+		public QuestNotice(String mainTxt, String subTxt, String sound)
+		{
 			this.startTime = Minecraft.getSystemTime();
 			this.mainTxt = mainTxt;
-
+			this.subTxt = subTxt;
 			this.sound = sound;
 		}
-
-		public float getTime() {
-			return (Minecraft.getSystemTime() - startTime) / 1000F;
+		
+		public float getTime()
+		{
+			return (Minecraft.getSystemTime() - startTime)/1000F;
 		}
 	}
 }
--- src/main/java/com/github/epiicthundercat/init/TheRaptureHandler.java
+++ src/main/java/com/github/epiicthundercat/init/TheRaptureHandler.java
@@ -1,6 +1,5 @@
 package com.github.epiicthundercat.init;
 
-import java.awt.Color;
 import java.util.Random;
 import java.util.Set;
 
@@ -8,29 +7,16 @@ import com.github.epiicthundercat.entity.monster.EntityFallenAngel;
 import com.google.common.collect.Sets;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.audio.PositionedSoundRecord;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.GlStateManager.DestFactor;
-import net.minecraft.client.renderer.GlStateManager.SourceFactor;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
-import net.minecraft.network.play.server.SPacketSoundEffect;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -43,23 +29,25 @@ public class TheRaptureHandler {
 	@SubscribeEvent
 	public void onTick(TickEvent.WorldTickEvent event) {
 		World world = event.world;
-		for (Entity e : world.loadedEntityList) {
-			if (e instanceof EntityPlayerMP) {
-				EntityPlayerMP player = (EntityPlayerMP) e;
-				player.connection.sendPacket(new SPacketSoundEffect(getSound(), SoundCategory.MASTER,
-						player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ(), 1F, 1F));
-			}
-		}
-
+		/*
+		 * for (Entity e : world.loadedEntityList) { if (e instanceof
+		 * EntityPlayerMP) { EntityPlayerMP player = (EntityPlayerMP) e;
+		 * player.connection.sendPacket(new SPacketSoundEffect(getSound(),
+		 * SoundCategory.MASTER, player.getPosition().getX(),
+		 * player.getPosition().getY(), player.getPosition().getZ(), 1F, 1F)); }
+		 * }
+		 */
 		if (event.phase == TickEvent.Phase.END && world.provider.getDimension() == 0) {
 			if (this.ticks % (10 * 20) == 0) {
 				Random random = new Random();
 				switch (random.nextInt(1)) {
 				case 0:
+					System.out.println("gogogo");
+					TheRaptureAnnouncement.ScheduleNotice("Raid Difficulty ", "Raiders have gotten harder!",
+							TheRaptureSoundHandler.THE_RAPTURE_HAS_BEGUN.getRegistryName().toString());
 
 					trySpawnAngel(world);
-					System.out.println("SpawningB");
-					this.onDrawScreen(event);
+
 					break;
 
 				}
@@ -75,16 +63,7 @@ public class TheRaptureHandler {
 		return TheRaptureSoundHandler.THE_RAPTURE_HAS_BEGUN;
 	}
 
-	 @SubscribeEvent
-	public void onDrawScreen(RenderGameOverlayEvent.Post event) {
-
-		
-		String tmp = TextFormatting.RED + "" + TextFormatting.UNDERLINE + "" + TextFormatting.BOLD
-				+ I18n.format("The Rapture is Coming");
-		
-	}
-
-	private void trySpawnAngel(World world) {
+	private static void trySpawnAngel(World world) {
 		Set<ChunkPos> eligibleChunksForSpawning = Sets.newHashSet();
 		int chunks = 0;
 
